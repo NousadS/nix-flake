6da146502d66621952191dd0fb17899e6e258d40
Nousad <nousad.cat@gmail.com>
2025-08-20 18:57:23 +0200

    patch: json to nix typo

--- homes/nousad/configs/vscode/keybinds.nix
+++ homes/nousad/configs/vscode/keybinds.nix
@@ -3,14 +3,14 @@
 {
     programs.vscode.keybindings = [
         {
-            key: "ctrl+[Backquote]";
-            command: "workbench.action.terminal.toggleTerminal";
-            when: "terminal.active";
+            key = "ctrl+[Backquote]";
+            command = "workbench.action.terminal.toggleTerminal";
+            when = "terminal.active";
         }
         {
-            key: "ctrl+/";
-            command: "workbench.action.addComment";
-            when: "activeCursorHasCommentingRange";
+            key = "ctrl+/";
+            command = "workbench.action.addComment";
+            when = "activeCursorHasCommentingRange";
         }
     ];
 }
