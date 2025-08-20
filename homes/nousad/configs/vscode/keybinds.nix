668ec13a24944b68ce1d43aa53d8456dfbeb641d
Nousad <nousad.cat@gmail.com>
2025-08-20 18:04:32 +0200

    add: vscode keybinds

--- homes/nousad/configs/vscode/keybinds.nix
+++ homes/nousad/configs/vscode/keybinds.nix
@@ -2,6 +2,15 @@
 
 {
     programs.vscode.keybindings = [
-        
+        {
+            key: "ctrl+[Backquote]";
+            command: "workbench.action.terminal.toggleTerminal";
+            when: "terminal.active"
+        }
+        {
+            key: "ctrl+/";
+            command: "workbench.action.addComment";
+            when: "activeCursorHasCommentingRange"
+        }
     ];
 }
