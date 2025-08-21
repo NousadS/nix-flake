2712f7d88fa7f512526adcae16c413b68dcfbda2
Nousad <nousad.cat@gmail.com>
2025-08-21 17:43:18 +0200

    patch: keyboard

--- homes/nousad/desktops/gnome/default.nix
+++ homes/nousad/desktops/gnome/default.nix
@@ -4,4 +4,12 @@
     programs.gnome-shell.extensions = with pkgs.gnomeExtensions; [
         user-themes
     ];
+
+    dconf.settings = {
+        "org/gnome/desktop/input-sources" = {
+        show-all-sources = true;
+        sources = [ (mkTuple [ "xkb" "eu" ]) (mkTuple [ "xkb" "us+altgr-intl" ]) ];
+        xkb-options = [ "terminate:ctrl_alt_bksp" ];
+        };
+    };
 }
