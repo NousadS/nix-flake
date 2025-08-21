0bc825b43fe241fc034d6d16b339bc9c53b8caed
Nousad <nousad.cat@gmail.com>
2025-08-21 18:16:56 +0200

    patch: typo

--- hosts/nousad/environment/desktop/xserver.nix
+++ hosts/nousad/environment/desktop/xserver.nix
@@ -1,25 +1,6 @@
 { config, lib, pkgs, ... }:
 
 {
-    imports = [
-        
-    ];
-
-    services.xserver.enable = true;
-    
-    services.xserver.displayManager.gdm.enable = true;
-    services.xserver.desktopManager.gnome.enable = true;
-
-
-    services.xserver.xkb = {
-        layout = "us,ru";
-        variant = "";
-        options = "grp:win_space_toggle";
-    };
-}
-{ config, lib, pkgs, ... }:
-
-{
     services.xserver.enable = true;
     
     services.xserver.displayManager.gdm.enable = true;
