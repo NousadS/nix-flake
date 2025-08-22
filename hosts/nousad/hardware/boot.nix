f883945e639dc4c0142a03d3356d26d841a7c354
Nousad <nousad.cat@gmail.com>
2025-08-23 01:24:36 +0200

    patch: grub

--- hosts/nousad/hardware/boot.nix
+++ hosts/nousad/hardware/boot.nix
@@ -1,6 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
-    boot.loader.systemd-boot.enable = true;
+    # boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
+
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "nodev";
+    boot.loader.grub.useOSProber = true;
+    boot.loader.grub.efiSupport = true;
 }
