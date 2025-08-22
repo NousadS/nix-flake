ba85851522678b22ab36b6a872bb447edf7d6677
Nousad <nousad.cat@gmail.com>
2025-08-23 01:21:39 +0200

    patch: grub customization

--- modules/system/themes/default/grub.nix
+++ modules/system/themes/default/grub.nix
@@ -1,16 +1,6 @@
-{ config, lib, pkgs, stylix, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-    boot.loader.grub.theme = pkgs.stdenv.mkDerivation {
-        pname = "distro-grub-themes";
-        version = "3.1";
-        src = pkgs.fetchFromGitHub {
-            owner = "AdisonCavani";
-            repo = "distro-grub-themes";
-            rev = "v3.1";
-            hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
-        };
-        installPhase = "cp -r customize/nixos $out";
-    };
+    boot.loader.grub.theme = pkgs.sleek-grub-theme;
 }
 
