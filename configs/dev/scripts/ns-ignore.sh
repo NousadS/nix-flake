6adb9f50644aa79831a3c2e0bcf36ad197ea4eb2
Nousad <nousad.cat@gmail.com>
2025-08-23 14:51:31 +0200

    patch: scripts

--- configs/dev/scripts/ns-ignore.sh
+++ configs/dev/scripts/ns-ignore.sh
@@ -9,11 +9,12 @@ fi
 TARGET_FILE="./.gitignore"
 LICENSE_NAME="$1"
 
-LICENSE_URL_NAME="$(printf '%s' "$2" | awk '{print toupper(substr($0,1,1)) substr($0,2)}')"
+LICENSE_URL_NAME="$(printf '%s' "$LICENSE_NAME" | awk '{print toupper(substr($0,1,1)) substr($0,2)}')"
 LICENSE_URL="https://raw.githubusercontent.com/github/gitignore/refs/heads/main/$LICENSE_URL_NAME.gitignore"
 
 curl -sSL "$LICENSE_URL" -o "$TARGET_FILE"
 
+echo "" >> "$TARGET_FILE"
 echo "# Automatically added by ns-license.sh" >> "$TARGET_FILE"
 echo "flake.nix" >> "$TARGET_FILE"
 echo "flake.lock" >> "$TARGET_FILE"
