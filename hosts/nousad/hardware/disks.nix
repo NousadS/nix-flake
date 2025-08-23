30a18537e03fb22f9c214c58f220f54d7bb9dd79
Nousad <nousad.cat@gmail.com>
2025-08-23 11:21:55 +0200

    patch: disk

--- hosts/nousad/hardware/disks.nix
+++ hosts/nousad/hardware/disks.nix
@@ -4,6 +4,6 @@
     fileSystems."/mnt/031342890D055036" = {
         device = "/dev/disk/by-uuid/031342890D055036";
         fsType = "auto";
-        options = [ "nosuid" "nodev" "nofail" "x-gvfs-show" ];
+        options = [ "noauto" "x-gvfs-show" "nosuid" "nodev" "nofail" ];
     };
 }
