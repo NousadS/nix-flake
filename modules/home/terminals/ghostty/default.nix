c67c87cb0f7a0505b31845ea2bfb8e8b021a344c
NousadS <nousad.cat@gmail.com>
2025-09-07 00:54:55 +0200

    patch: gruvbox ghostty theme

--- modules/home/terminals/ghostty/default.nix
+++ modules/home/terminals/ghostty/default.nix
@@ -20,31 +20,7 @@ mkModule {
 
                 keybind = f11=toggle_fullscreen
 
-                background = ${config.lib.stylix.colors.base00}
-                foreground = ${config.lib.stylix.colors.base07}
-
-                selection-background = ${config.lib.stylix.colors.base03}
-                selection-foreground = ${config.lib.stylix.colors.base00}
-
-                cursor-color = ${config.lib.stylix.colors.base05}
-                cursor-text = ${config.lib.stylix.colors.base07}
-
-                palette = 0=${config.lib.stylix.colors.base00}
-                palette = 1=${config.lib.stylix.colors.base01}
-                palette = 2=${config.lib.stylix.colors.base02}
-                palette = 3=${config.lib.stylix.colors.base03}
-                palette = 4=${config.lib.stylix.colors.base04}
-                palette = 5=${config.lib.stylix.colors.base05}
-                palette = 6=${config.lib.stylix.colors.base06}
-                palette = 7=${config.lib.stylix.colors.base07}
-                palette = 8=${config.lib.stylix.colors.base08}
-                palette = 9=${config.lib.stylix.colors.base09}
-                palette = 10=${config.lib.stylix.colors.base10}
-                palette = 11=${config.lib.stylix.colors.base11}
-                palette = 12=${config.lib.stylix.colors.base12}
-                palette = 13=${config.lib.stylix.colors.base13}
-                palette = 14=${config.lib.stylix.colors.base14}
-                palette = 15=${config.lib.stylix.colors.base15}
+                theme = GruvboxDarkHard
             '';
         }; 
         
