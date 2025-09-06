346c756b47b9bc55f39728660db81ea4d6d5e422
NousadS <nousad.cat@gmail.com>
2025-09-07 00:37:59 +0200

    patch: gruvbox theme

--- modules/host/themes/default/colors.nix
+++ modules/host/themes/default/colors.nix
@@ -8,10 +8,5 @@
     #     if config.modules.host.themes.default.theme == null
     #     then "${pkgs.base16-schemes}/share/themes/eris.yaml"
     #     else "${pkgs.base16-schemes}/share/themes/" + config.modules.host.themes.default.theme + ".yaml";
-
-    stylix.base16Overrides = {
-        base05 = config.lib.stylix.colors.base05;
-        base03 = config.lib.stylix.colors.base05;
-    };
 }
 
