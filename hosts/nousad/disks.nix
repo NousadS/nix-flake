928749150b3944d376b6c5c10435845ef375cd6c
NousadS <nousad.cat@gmail.com>
2025-10-25 13:02:32 +0200

    patch: partitions

--- hosts/nousad/disks.nix
+++ hosts/nousad/disks.nix
@@ -3,14 +3,40 @@
 {
     fileSystems."/mnt/Projects" = {
         device = "/dev/disk/by-uuid/031342890D055036";
-        fsType = "auto";
+        fsType = "ntfs";
         options = [
             "noauto"
-            "x-gvfs-show"
             "nosuid"
             "nodev"
             "nofail"
+            "x-gvfs-show"
             "x-gvfs-name=Projects"
+        ];
+    };
+
+    fileSystems."/mnt/Data" = {
+        device = "/dev/disk/by-uuid/1A65BE83562E7DCE";
+        fsType = "ntfs";
+        options = [
+            "noauto"
+            "nosuid"
+            "nodev"
+            "nofail"
+            "x-gvfs-show"
+            "x-gvfs-name=Data"
+        ];
+    };
+
+    fileSystems."/mnt/Windows" = {
+        device = "/dev/disk/by-uuid/1AEC0FF5EC0FCA49";
+        fsType = "ntfs";
+        options = [
+            "noauto"
+            "nosuid"
+            "nodev"
+            "nofail"
+            "x-gvfs-show"
+            "x-gvfs-name=Windows"
         ];
     };
 
