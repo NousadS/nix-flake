b1a8c7b3d358e74de70388d6932063732cbc35b3
NousadS <nousad.cat@gmail.com>
2025-11-06 22:24:23 +0100

    patch: git

--- modules/host/commands/git/default.nix
+++ modules/host/commands/git/default.nix
@@ -1,10 +1,11 @@
 { config, lib, pkgs, mkModule, ... } @ args:
 
-mkModule {  programs.git = {
-    enable = true;
-    userName  = "John Doe";
-    userEmail = "johndoe@example.com";config = {
+mkModule {
+    programs.git = {
+        enable = true;
+    #     userName  = "John Doe";
+    #     userEmail = "johndoe@example.com";config = {
 
+    #     };
     };
-  };
 }
