a00d43a18ce53d378bd49f9354d34ed442b38593
NousadS <nousad.cat@gmail.com>
2025-11-09 00:54:47 +0100

    add: new modules structure

--- homes/nousad/modules.nix
+++ homes/nousad/modules.nix
@@ -1,26 +1,9 @@
 { config, lib, pkgs, self, ... }:
 
 {
-    imports = [
-        # ../../modules/core-home/...
-
-        (self + "/modules/home")
-    ];
+    imports = [ (self + "/modules/home") ];
 
     modules.home = {
-        # desktops.gnome.enable = true;
-        
-        # editors.neovim.enable = true;
-        # editors.vscode.enable = true;
-
-        # fetches.fastfetch.enable = true;
-
-        # nousad.scripts.enable = true;
-
-        # services.solaar.enable = true;
-
-        # shells.zsh.enable = true;
-
-        # terminals.ghostty.enable = true;
+        commands.neovim.enable = false;
     };
 }
--- hosts/laptop/modules.nix
+++ hosts/laptop/modules.nix
@@ -1,14 +1,21 @@
 { config, lib, pkgs, self, ... }:
 
 {
-    imports = [
-        "${self}/modules/host"
-    ];
+    imports = [ (self + "/modules/host") ];
 
     modules.host = {
-        bootloader.systemd.enable = false;
-        commands.alejandra.enable = true;
+        bootloader.grub.enable = false;
+
+        apps.alacritty.enable = false;
+        apps.firefox.enable = false;
+        apps.kdenlive.enable = false;
+        apps.logseq.enable = false;
+        apps.openshot.enable = false;
+        apps.zoom.enable = false;
+
+        commands.devenv.enable = false;
+        commands.neovim.enable = false;
+
         keyboards.short.enable = false;
-        editors.logseq.enable = false;
     };
 }
