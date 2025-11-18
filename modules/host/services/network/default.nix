ab444d6339875831fc26bdeeb1bc352fac095dbd
NousadS <nousad.cat@gmail.com>
2025-11-18 23:02:27 +0100

    patch: some experiments with firewall

--- modules/host/services/network/default.nix
+++ modules/host/services/network/default.nix
@@ -7,4 +7,21 @@
 } @ args:
 mkModule {
     networking.networkmanager.enable = true;
+    networking.nftables.enable = true;
+
+    networking.firewall = {
+        enable = true;
+
+        allowPing = true;
+        pingLimit = "2/minute";
+
+        logRefusedPackets = true;
+        logReversePathDrops = true;
+        logRefusedConnections = true;
+        logRefusedUnicastsOnly = true;
+
+        interfaces = {
+            "wlo1" = {};
+        };
+    };
 }
