12cee0eb00f670932cd2656f5e3c35d430e386e1
NousadS <nousad.cat@gmail.com>
2025-11-23 04:33:56 +0100

    patch: new modules structure

--- hosts/laptop/modules.nix
+++ hosts/laptop/modules.nix
@@ -8,18 +8,15 @@
     imports = [(self + "/modules/host")];
 
     modules.host = {
-        bootloader.grub.enable = false;
+        system.systemd.enable = false;
+        system.grub.enable = true;
 
-        apps.alacritty.enable = false;
         apps.firefox.enable = false;
         apps.kdenlive.enable = false;
-        apps.logseq.enable = false;
         apps.openshot.enable = false;
         apps.zoom.enable = false;
 
         commands.devenv.enable = false;
         commands.neovim.enable = false;
-
-        keyboards.short.enable = false;
     };
 }
