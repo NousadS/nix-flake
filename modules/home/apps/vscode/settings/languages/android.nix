6b028a9cb088ceea39a4fb38aa931ae1de01f612
NousadS <nousad.cat@gmail.com>
2026-01-19 05:12:47 +0100

    patch: hashes

--- modules/home/apps/vscode/settings/languages/android.nix
+++ modules/home/apps/vscode/settings/languages/android.nix
@@ -15,13 +15,13 @@
                 publisher = "fwcd";
                 name = "kotlin";
                 version = "0.2.36";
-                sha256 = "sha256-RB5YOp30tfMEzGyXpOwPIHzXqZlRGc+pXiJ3foego7Y=";
+                sha256 = "sha256-tCpxFWSQZNhiHdJyxSbQ1QakS2jNqWQrA2/grLZklrM=";
             }
             {
                 publisher = "cstef";
                 name = "kotlin-formatter";
                 version = "0.0.9";
-                sha256 = "sha256-PbXhOsoR0/5wXuFrzwCcauM1uGgfQoSRTj0gPVVZ4Kg=";
+                sha256 = "sha256-V+abyEzT790sfXvI+GnYcIWiWZ56xpCs/Bw0/JWtLHA=";
             }
         ];
 }
